55c41ebcbbcad6979d003b84588fcfb3dc9daaf6
Antoine AMARA <amara.antoine@gmail.com>
2020-12-04 21:38:34 +0000

    feat(parser): parse line with multiple elements by separator
    parse a file with multiple element as an array of array of string.
    pass a separator to split each line and have one element by index

--- advent_of_code_2020/helpers/fileparsers.py
+++ advent_of_code_2020/helpers/fileparsers.py
@@ -4,3 +4,11 @@ def parse1DArrayToInt(filepath):
         for line in file:
             parsedInput.append(int(line))
     return parsedInput
+
+
+def parse2DArrayToString(filepath, separator=","):
+    parsedInput = []
+    with open(filepath, "r+") as file:
+        for line in file:
+            parsedInput.append(line.split(separator))
+    return parsedInput
